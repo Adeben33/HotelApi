bca0dd2f0fe02aa4c9ed40de2dc7e68dfa29df81
adeben33 <adeniyiebenezer33@gmail.com>
2023-01-11 23:57:28 +0100

    apartment struct

--- internals/entity/userEntity/entity.go
+++ internals/entity/userEntity/entity.go
@@ -10,7 +10,15 @@ type CreateUser struct {
 	Gender        string `json:"gender" validate:"required, oneof='male' 'female' Others"`
 	DateOfBirth   string `json:"dateOfBirth" validate:"omitempty"`
 	AccountStatus string `json:"accountStatus" validate:"required oneof='active' 'paused' 'deactivated'"`
-	BookedApartment
+	role          string `json:"role" validate:"required"`
+}
+
+type apartment struct {
+	ApartmentId     string `json:"apartment_id" validate:"required"`
+	ApartmentStatus string `json:"apartment_status" validate:"required oneof='available' 'booked' 'paused'"`
+	DaysBookedfor   int    `json:"days_bookedfor" validate:"omitempty""`
+	Occupant        string `json:"user_id" validate:"required"`
+	CreatedBy       string `json:"created_by" validate:"required"`
 }
 
 //type createUser struct {
