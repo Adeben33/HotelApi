21431de5511bb339675d5ba60e11e7ca996d88cf
adeben33 <adeniyiebenezer33@gmail.com>
2023-01-23 12:48:14 +0100

    review handler

---
+++ cmd/handlers/reviewHandler/handler.go
@@ -0,0 +1,7 @@
+package reviewHandler
+
+import "github.com/gin-gonic/gin"
+
+func GetAllReviews(c *gin.Context) {
+
+}
--- cmd/routes/review.go
+++ cmd/routes/review.go
@@ -1,18 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/adeben33/HotelApi/cmd/handlers/reviewHandler"
+	"github.com/gin-gonic/gin"
+)
 
 func ReviewsRoutes(route *gin.Engine) {
-	route.GET("/reviews", handlers.GetAllReviews)
-	route.GET("/reviews/:id", handlers.GetAReview)
-	route.POST("/reviews", handlers.CreateReview)
-	route.PUT("/reviews/:id", handlers.UpdateReview())
-	route.DELETE("/reviews/:id", handlers.DeleteReview())
-	//	Retrieve a list of reviews for a specific apartment.
-	route.GET("/reviews", handlers.GetAnApartmentReview)
-	//Retrieve a list of reviews written by a specific user.
-	route.GET("/reviews", handlers.GetAReview)
-	//Retrieve a list of reviews with a specific rating.
-	route.GET("/reviews", handlers.GetASpecificReviewWithRating())
+	route.GET("/reviews", reviewHandler.GetAllReviews)
+	//route.GET("/reviews/:id", reviewHandler.GetAReview)
+	//route.POST("/reviews", reviewHandler.CreateReview)
+	//route.PUT("/reviews/:id", reviewHandler.UpdateReview())
+	//route.DELETE("/reviews/:id", reviewHandler.DeleteReview())
+	////	Retrieve a list of reviews for a specific apartment.
+	//route.GET("/reviews", reviewHandler.GetAnApartmentReview)
+	////Retrieve a list of reviews written by a specific user.
+	//route.GET("/reviews", reviewHandler.GetAReview)
+	////Retrieve a list of reviews with a specific rating.
+	//route.GET("/reviews", reviewHandler.GetASpecificReviewWithRating())
 
 }
