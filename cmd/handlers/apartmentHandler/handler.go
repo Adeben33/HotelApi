32ea340b14d6e935b75b27235dddbf3d1da678cb
adeben33 <adeniyiebenezer33@gmail.com>
2023-01-25 23:48:40 +0100

    apartment struct

--- cmd/handlers/apartmentHandler/handler.go
+++ cmd/handlers/apartmentHandler/handler.go
@@ -1,7 +1,28 @@
 package apartmentHandler
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/adeben33/HotelApi/internals/dataBaseStore/mongoDBConnection"
+	"github.com/adeben33/HotelApi/internals/entity"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"time"
+)
+
+var validate = validator.New()
+var apartmentCollection = mongoDBConnection.OpenCollection(mongoDBConnection.Client, "apartment")
 
 func GetAllApartment(c *gin.Context) {
+}
+
+func CreateApartment(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	var apartment entity.Apartment
+	if err := c.BindJSON(&apartment); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while bindind data"})
+		return
+	}
 
 }
--- internals/entity/apartment.go
+++ internals/entity/apartment.go
@@ -3,21 +3,21 @@ package entity
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Apartment struct {
-	ID          primitive.ObjectID
-	Name        string
-	Address     AddressInfo
-	Amenities   []string
-	Images      []string
-	Price       uint16
-	Review      []primitive.ObjectID
-	Bookings    []primitive.ObjectID
-	RenterId    primitive.ObjectID
-	ApartmentId primitive.ObjectID
+	ID          primitive.ObjectID   `json:"_id" bson:"_id"`
+	Name        string               `json:"name" bson:"name" validate:"required"`
+	Address     AddressInfo          `json:"address" bson:"address" validate:"omitempty"`
+	Amenities   []string             `json:"amenities" bson:"amenities" validate:"omitempty"`
+	Images      []string             `json:"images" bson:"images" validate:"omitempty"`
+	Price       uint16               `json:"price" bson:"price" validate:"required,number"`
+	Review      []primitive.ObjectID `json:"review" bson:"review" validate:"omitempty"`
+	Bookings    []primitive.ObjectID `json:"bookings" bson:"bookings" validate:"omitempty"`
+	RenterId    primitive.ObjectID   `json:"renterId" bson:"renter_id"`
+	ApartmentId primitive.ObjectID   `json:"apartmentId" bson:"apartment_id"`
 }
 
 type AddressInfo struct {
-	Street string
-	City   string
-	State  string
-	Zip    string
+	Street string `json:"street" bson:"street" validate:"required"`
+	City   string `json:"city" bson:"city" validate:"required"`
+	State  string `json:"state" bson:"state" validate:"required"`
+	Zip    string `json:"zip" bson:"zip" validate:"omitempty"`
 }
